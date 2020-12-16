9abbe1919a638fab5c4dd35b4045ab0e8b64e661
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-16 09:33:52 -0300

    Add Readme

---
+++ teste-unitario-youtube/MyClassTest/FileProcessTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyClassTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            //TODO
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            //TODO
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmptyThrowNewArgumentNullException()
+        {
+            //TODO
+            Assert.Inconclusive();
+        }
+
+
+    }
+}
---
+++ teste-unitario-youtube/README.md
@@ -0,0 +1,2 @@
+# unit-test >> Curso Youtube
+Link : https://www.youtube.com/watch?v=zFYR_6XVr4M&list=PLWNaqtzH6CWRUd0iIEJotRrpwtXXnFROE&ab_channel=ProgrAmadaMente
