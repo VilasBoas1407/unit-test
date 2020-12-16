69ee8543e2dc5c65dd0e54e11a66b704d86b2e61
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-16 09:32:41 -0300

    Adicioando projeto

---
+++ teste-unitario-youtube/MyClassTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyClassTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
