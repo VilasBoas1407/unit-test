beadff78c172bdfe4aba85b9d308519e5b4a8ca8
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-16 10:45:14 -0300

    Finalizando Testes Básicos

--- teste-unitario-youtube/MyClassTest/FileProcessTest.cs
+++ teste-unitario-youtube/MyClassTest/FileProcessTest.cs
@@ -1,19 +1,35 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyClass;
+using System;
+using System.Configuration;
+using System.IO;
 
 namespace MyClassTest
 {
     [TestClass]
     public class FileProcessTest
     {
+        private const string BAD_FILE_NAME = @"C:\Regedit.exe";
+        private string _GoodFileName;
+
+        public void SetGoodFileName()
+        {
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if (_GoodFileName.Contains("[AppPath]"))
+            {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]", 
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
         [TestMethod]
         public void FileNameDoesExists()
         {
             FileProcess fp = new FileProcess();
             bool fromCall;
-
-            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
-
+            SetGoodFileName();
+            File.AppendAllText(_GoodFileName, "Some Text");
+            fromCall = fp.FileExists(_GoodFileName);
+            File.Delete(_GoodFileName);
             Assert.IsTrue(fromCall);
 
         }
@@ -24,17 +40,35 @@ namespace MyClassTest
             FileProcess fp = new FileProcess();
             bool fromCall;
 
-            fromCall = fp.FileExists(@"C:\Regedit.exe");
+            fromCall = fp.FileExists(BAD_FILE_NAME);
 
             Assert.IsFalse(fromCall);
 
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void FileNameNullOrEmptyThrowNewArgumentNullException()
         {
-            //TODO
-            Assert.Inconclusive();
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmptyThrowNewArgumentNullExceptionUsingTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+
+            Assert.Fail("Fail expected");
         }
 
     }
